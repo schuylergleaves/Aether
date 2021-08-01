77df3f5c568ff9fbdb13b9e57132108749a46e82
Schuyler Gleaves <sgleaves@microsoft.com>
2021-07-31 17:06:06 -0700

    Added makefile updates

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-cd ../build
-/opt/cross/bin/i686-elf-as ../kernel/arch/i386/boot.s -o boot.o
-/opt/cross/bin/i686-elf-g++ -c ../kernel/core/kernel.cc -o kernel.o -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti
-/opt/cross/bin/i686-elf-g++ -c ../kernel/terminal/terminal.cc -o terminal.o -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti
-/opt/cross/bin/i686-elf-g++ -c ../kernel/libc/string/strlen.cc -o strlen.o -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti
-/opt/cross/bin/i686-elf-g++ -c ../kernel/libc/stdio/printf.cc -o printf.o -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti
-
-
-mkdir -p isodir/boot/grub
-
-/opt/cross/bin/i686-elf-g++ -T ../kernel/arch/i386/linker.ld -o isodir/boot/aether.bin -ffreestanding -O2 -nostdlib boot.o kernel.o terminal.o strlen.o printf.o -lgcc
-
-cp ../kernel/arch/i386/grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o aether.iso isodir/
+cd ../kernel
+make
